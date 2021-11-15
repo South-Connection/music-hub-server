397565422cc391bd860343650f0d893e3fa1db4d
Kati Fe <k-felgitsch@hotmail.com>
2021-11-15 11:10:54 +0000

    cleaning auth.js / changing error messages

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,16 @@
 const router = require("express").Router();
 
-// ℹ️ Handles password encryption
+
 const bcrypt = require("bcryptjs");
 const mongoose = require("mongoose");
 
-// How many rounds should bcrypt run the salt (default [10 - 12 rounds])
+
 const saltRounds = 10;
 
-// Require the User model in order to interact with the database
+
 const User = require("../models/User.model");
 
-// Require necessary (isLoggedOut and isLiggedIn) middleware in order to control access to specific routes
+
 const isLoggedOut = require("../middleware/isLoggedOut");
 const isLoggedIn = require("../middleware/isLoggedIn");
 
@@ -44,26 +44,25 @@ router.post("/signup", isLoggedOut, (req, res) => {
   }
 
 
-  // Search the database for a user with the username submitted in the form
   User.findOne({ username }).then((found) => {
-    // If the user is found, send the message username is taken & redirect to /signup
+   
     if (found) {
-      // return res.status(400).json("auth/signup", { errorMessage: "Username already taken." });
+      return res.status(400).json("auth/signup", { errorMessage: "Username already taken." });
     }
 
-    // if user is not found, create a new user - start with hashing the password
+   
     return bcrypt
       .genSalt(saltRounds)
       .then((salt) => bcrypt.hash(password, salt))
       .then((hashedPassword) => {
-        // Create a user and save it in the database
+        
         return User.create({
           username,
           password: hashedPassword,
         });
       })
       .then((user) => {
-        // Bind the user to the session object
+      
         req.session.user = user;
         res.status(201).json(user);
       })
@@ -91,38 +90,35 @@ router.post("/login", isLoggedOut, (req, res, next) => {
       .json({ errorMessage: "Please provide your username." });
   }
 
-  // Here we use the same logic as above
-  // - either length based parameters or we check the strength of a password
+ 
   if (password.length < 8) {
     return res.status(400).json({
       errorMessage: "Your password needs to be at least 6 characters long.",
     });
   }
 
-  // Search the database for a user with the username submitted in the form
+  
   User.findOne({ username })
     .then((user) => {
-      // If the user isn't found, send the message that user provided wrong credentials
+     
       if (!user) {
         return res.status(400).json({ errorMessage: "Wrong credentials." });
       }
 
-      // If user is found based on the username, check if the in putted password matches the one saved in the database
+      
       bcrypt.compare(password, user.password).then((isSamePassword) => {
         if (!isSamePassword) {
           return res.status(400).json({ errorMessage: "Wrong credentials." });
         }
         req.session.user = user;
-        // req.session.user = user._id; // ! better and safer but in this case we saving the entire user object
+        
         return res.json(user);
       });
     })
 
     .catch((err) => {
-      // in this case we are sending the error handling to the error handling middleware that is defined in the error handling file
-      // you can just as easily run the res.status that is commented out below
       next(err);
-      // return res.status(500).render("login", { errorMessage: err.message });
+      return res.status(500).render("login", { errorMessage: err.message });
     });
 });
 
@@ -131,7 +127,7 @@ router.get("/logout", isLoggedIn, (req, res) => {
     if (err) {
       return res.status(500).json({ errorMessage: err.message });
     }
-    res.json({ message: "Your are logged out" });
+    res.json({ message: "Log out successfully!" });
   });
 });
 
