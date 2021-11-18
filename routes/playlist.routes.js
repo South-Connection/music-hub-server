e8a72e2cc511a789c9d2a3047678268caaeee0f3
Geoffrey Laville <laville.geoffrey@gmail.com>
2021-11-18 21:57:22 +0100

    functionality to add songs in playlists details

--- routes/playlist.routes.js
+++ routes/playlist.routes.js
@@ -28,11 +28,29 @@ router.get("/users", (req, res, next) => {
     .catch((err) => res.json(err));
 });
 
-//get all the playlists
-router.get("/playlists", (req, res, next) => {
+//get all the playlists in the system
+router.get("/all-playlists", (req, res, next) => {
   Playlist.find()
     // .populate()
     .then((allThePlaylists) => res.json(allThePlaylists))
+    .catch((err) => res.json(err));
+});
+
+//get all the playlists for the current user (user needs to be owner or guest)
+router.get("/playlists", (req, res, next) => {
+  //todo: get only the playlists related to the current user
+  // current user: req.session.user._id (?)
+  //
+
+  Playlist.find()
+    // .populate()
+    .then((allThePlaylists) => {
+      //filter....
+      // const myPlaylists = allThePlaylists.filter((item) => {
+      //   return xxxx === xxxxxx || item.type === this.state.filter;
+      // });
+      res.json(allThePlaylists);
+    })
     .catch((err) => res.json(err));
 });
 
@@ -54,19 +72,30 @@ router.get("/playlists/:playlistId", (req, res, next) => {
 //update Playlist
 router.put("/playlists/:playlistId", (req, res, next) => {
   const { playlistId } = req.params;
+  const newDetails = req.body;
+
+  console.log(newDetails);
 
   if (!mongoose.Types.ObjectId.isValid(playlistId)) {
     res.status(400).json({ message: "Specified id is not valid" });
     return;
   }
 
-  Playlist.findByIdAndUpdate(playlistId, req.body)
+  Playlist.findByIdAndUpdate(playlistId, newDetails)
     .then(() =>
       res.json({
         message: `Playlist with ${playlistId} is updated successfully.`,
       })
     )
     .catch((err) => res.status(500).json(err));
+
+  // Playlist.findByIdAndUpdate(playlistId, req.body, {songs: [...{ title: songName, link: songLink }]})
+  //   .then(() =>
+  //     res.json({
+  //       message: `Playlist with ${playlistId} is updated successfully.`,
+  //     })
+  //   )
+  //   .catch((err) => res.status(500).json(err));
 });
 
 //delete Playlist
